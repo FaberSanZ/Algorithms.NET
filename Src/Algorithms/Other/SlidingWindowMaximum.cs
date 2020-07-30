0e3fb2c313ed3c2fbe50e49a2b955d9eacd3247f
Faber Leonardo Sanchez Hidalgo <FaberSanZ@users.noreply.github.com>
2020-07-30 16:00:32 -0500

    Implement SquareRootDecomposition

---
+++ Src/Algorithms/Other/SlidingWindowMaximum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Other
+{
+    public class SlidingWindowMaximum
+    {
+        int[] values;
+        public int N, lo, hi;
+
+        List<int> deque = new List<int>();
+
+        //public SlidingWindowMaximum(int[] values)
+        //{
+        //    if (values == null) throw new Exception();
+        //    this.values = values;
+        //    N = values.Length;
+        //}
+
+        //// Advances the front of the window by one unit
+        //public void advance()
+        //{
+
+        //    // Remove all the worse values in the back of the deque
+        //    while (!(deque.Count is 0 ) && values[deque.las()] < values[hi])
+        //        deque.removeLast(); // Change the '<' sign here ^^^ to '>' for minimum sliding window
+
+        //    // Add the next index to the back of the deque
+        //    deque.addLast(hi);
+
+        //    // Increase the window size
+        //    hi++;
+        //}
+
+        //// Retracks the back of the window by one unit
+        //public void shrink()
+        //{
+
+        //    // Decrease window size by pushing it forward
+        //    lo++;
+
+        //    // Remove elements in the front of the queue whom are no longer
+        //    // valid in the reduced window.
+        //    while (!deque.isEmpty() && deque.peekFirst() < lo) deque.removeFirst();
+        //}
+
+        //// Query the current maximum value in the window
+        //public int getMax()
+        //{
+        //    if (lo >= hi) throw new IllegalStateException("Make sure lo < hi");
+        //    return values[deque.peekFirst()];
+        //}
+    }
+}
---
+++ Src/Algorithms/Other/SquareRootDecomposition.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Other
+{
+    public class SquareRootDecomposition
+    {
+        internal long[] arr, blocks;
+        internal int blockSize, nBlocks;
+
+        // Creates an empty range [0,n]
+        public SquareRootDecomposition(int size)
+        {
+
+            blockSize = (int)Math.Sqrt(size);
+            nBlocks = (size / blockSize) + 1;
+            blocks = new long[nBlocks];
+            arr = new long[size];
+        }
+
+        public SquareRootDecomposition(int[] values) : this(values.Length)
+        {
+
+            for (int i = 0; i < values.Length; i++) Set(i, values[i]);
+        }
+
+        public int BlockID(int index)
+        {
+            return index / blockSize;
+        }
+
+        // Sets [index, index] = val
+        public void Set(int index, int val)
+        {
+            blocks[BlockID(index)] -= arr[index];
+            blocks[BlockID(index)] += val;
+            arr[index] = val;
+        }
+
+        // Get sum query from [lo, hi] in O(sqrt(n))
+        public long Query(int lo, int hi)
+        {
+
+            long sum = 0;
+            int loId = BlockID(lo);
+            int hiId = BlockID(hi);
+            for (int i = loId + 1; i < hiId; i++) sum += blocks[i];
+
+            if (loId == hiId)
+            {
+                for (int i = lo; i <= hi; i++) sum += arr[i];
+                return sum;
+            }
+
+            int loMax = (((lo / blockSize) + 1) * blockSize) - 1;
+            int hiMin = (hi / blockSize) * blockSize;
+            for (int i = lo; i <= loMax; i++) sum += arr[i];
+            for (int i = hiMin; i <= hi; i++) sum += arr[i];
+
+            return sum;
+        }
+    }
+}
